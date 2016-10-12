51d55ff710c05b5b55758a933def70d5340b64c5
Omar Alaa <omaralaa14895@gmail.com>
2016-10-12 13:08:48 +0200

    Fixed branch decider

--- BranchDecider.vhd
+++ BranchDecider.vhd
@@ -1,40 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    20:22:08 10/08/2016 
--- Design Name: 
--- Module Name:    BranchDecider - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity BranchDecider is
-    Port ( PREVIOUS_STAGE : in  STD_LOGIC_VECTOR (31 DOWNTO 0);
-           NEXT_PC: in  STD_LOGIC_VECTOR (31 DOWNTO 0);
+    Port ( NEXT_PC: in  STD_LOGIC_VECTOR (31 DOWNTO 0);
 			  SIGN_EXTEND: in  STD_LOGIC_VECTOR (31 downto 0);
-           SEL : in  STD_LOGIC;
-           OUTPUT : out  STD_LOGIC_VECTOR (31 DOWNTO 0) );
+           A : in  STD_LOGIC_VECTOR(31 DOWNTO 0);
+           OUTPUT : out STD_LOGIC_VECTOR (31 DOWNTO 0) );
 end BranchDecider;
 
 architecture Behavioral of BranchDecider is
@@ -60,11 +32,11 @@ architecture Behavioral of BranchDecider is
 	signal adder_cout: STD_LOGIC;
 	constant adder_operation: STD_LOGIC := '0';
 	signal result: STD_LOGIC_VECTOR (31 DOWNTO 0);
+	signal SEL: STD_LOGIC;
 	
-	
-begin
-	
-		adderModule: ADD_SUB port map(PREVIOUS_STAGE, SIGN_EXTEND, adder_operation , adder_cout,adder_output);
+begin	
+		SEL <= '1' WHEN (A = x"0000000") else '0';		
+		adderModule: ADD_SUB port map(NEXT_PC, SIGN_EXTEND, adder_operation , adder_cout,adder_output);
 		muxModule: MUX32b port map (adder_output, NEXT_PC, SEL , result);
 		OUTPUT <= result;
 
